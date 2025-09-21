603232cbfe3975a5531eca501a6098e5d3fb573b
Grozi-rex <goodlucknnamdi46@gmail.com>
2025-09-21 18:25:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import settings from './settings.js';
 export default {
   // Bot configuration
   prefix: '.', // input your prfic here 
-  ownerNumber: '', //input your number here 
+  ownerNumber: '2347079455660', //input your number here 
   botName: '✦✦✦ 𝐇 𝐎 𝐑 𝐋 𝐀 𝐏 𝐎 𝐎 𝐊 𝐈 𝐄 ✦✦✦',
-  ownerName: '𝓗𝓞𝓡𝓛𝓐𝓟𝓞𝓞𝓚𝓘𝓔', //optional 
+  ownerName: 'HORLA-POOKIE-SESSION-ID'', //optional 
   sessionId: 'HORLA-POOKIE-SESSION-ID', //sensitive name 
   BOOM_MESSAGE_LIMIT: 50,  
 
